1f6fc3bf949c8ce8b27b17f3fda5a64f643408be
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-10 17:53:01 +0200

    Implement logic to find and display the lightest bike from the array

--- 01-snack/script.js
+++ 01-snack/script.js
@@ -22,4 +22,21 @@ const bikes = [
         weight: 12
     }
 
-]
+];
+
+console.log(bikes);
+console.log(bikes[0].weight);
+
+// per sapere quale bici pesa di meno devo avere un valore di partenza da confrontare con tutti gli altri valori
+// inizializzo una variabile con il primo oggetto dell'array
+let lightestBike = bikes[0];
+// faccio un ciclo for per confrontare il peso di ogni bici
+for (let i = 1; i < bikes.length; i++) {
+    const thisBike = bikes[i];
+    if (thisBike.weight < lightestBike.weight) {
+        lightestBike = thisBike;
+
+    }
+};
+// stampo la bici con il peso minore e il valore corrispondente
+console.log(`La bici con il peso minore è la ${lightestBike.name}, con un peso di ${lightestBike.weight} kg.`);
