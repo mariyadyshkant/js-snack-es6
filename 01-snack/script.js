137281235e5b1970fd8429875621b43215675422
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-10 17:25:44 +0200

    Add array of bike objects with properties for name and weight

---
+++ 01-snack/script.js
@@ -0,0 +1,25 @@
+// Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+const bikes = [
+    {
+        name: "SuperTurbo",
+        weight: 8 // kg
+    },
+    {
+        name: "RoadRunner",
+        weight: 7
+    },
+    {
+        name: "MountainKing",
+        weight: 9.7
+    },
+    {
+        name: "MiniBiky",
+        weight: 5.8
+    },
+    {
+        name: "ElectroSpeed",
+        weight: 12
+    }
+
+]
